b113438ebdb6ef8b7cb4328ca10873216744276c
himmelmaus <>
2020-12-06 05:10:21 +0000

    Slight change to make them 1 liners

--- day_6/main.py
+++ day_6/main.py
@@ -1,7 +1,7 @@
 with open("input.txt", "r") as infile:
     lines = infile.read().split("\n\n")
 
-total_count = 0
-for line in lines:
-    total_count += sum([1 for character in "abcdefghijklmnopqrstuvwxyz" if all([character in subline for subline in line.split("\n")])])
-print(total_count)
+count1 = sum([sum([1 for character in "abcdefghijklmnopqrstuvwxyz" if character in line]) for line in lines])
+count2 = sum([sum([1 for character in "abcdefghijklmnopqrstuvwxyz" if all([character in subline for subline in line.split("\n")])]) for line in lines])
+
+print(count1, count2)
