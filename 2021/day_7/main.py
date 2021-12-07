bfbe1eb80a0ff1db15d4dd403004865ce429ee84
Himmelmaus <fsmith@novarumdx.com>
2021-12-07 06:00:15 +0000

    quick update because I have brainworms

--- 2021/day_7/main.py
+++ 2021/day_7/main.py
@@ -1,14 +1,11 @@
 from functools import reduce
 import numpy as np
 
-
-crabbies = np.array(list(map(int, open("input.txt").read().split(","))))
+crabbies = np.array(list(map(int, open("test_input.txt").read().split(","))))
 positions = np.array(range(0, max(crabbies)))
-
 
 print("part 1")
 print(min(map(lambda x: sum(np.abs(crabbies - x)), positions)))
 
 print("part 2")
-tri_sum = lambda x: reduce(lambda a, b: a+b, range(x+1)) if x!= 0 else 0
-print(min(map(lambda x: sum(map(tri_sum, np.abs(crabbies - x))), positions)))
+print(min(map(lambda x: sum(map(lambda x: sum(range(x+1)), np.abs(crabbies - x))), positions)))
