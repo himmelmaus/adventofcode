775023135ccc2801fcc433d55a29d0fa3a88161d
Himmelmaus <fsmith@novarumdx.com>
2021-12-07 06:01:18 +0000

    make em one liners baybeeeee

--- 2021/day_7/main.py
+++ 2021/day_7/main.py
@@ -2,10 +2,9 @@ from functools import reduce
 import numpy as np
 
 crabbies = np.array(list(map(int, open("test_input.txt").read().split(","))))
-positions = np.array(range(0, max(crabbies)))
 
 print("part 1")
-print(min(map(lambda x: sum(np.abs(crabbies - x)), positions)))
+print(min(map(lambda x: sum(np.abs(crabbies - x)), range(0, max(crabbies)))))
 
 print("part 2")
-print(min(map(lambda x: sum(map(lambda x: sum(range(x+1)), np.abs(crabbies - x))), positions)))
+print(min(map(lambda x: sum(map(lambda x: sum(range(x+1)), np.abs(crabbies - x))), range(0, max(crabbies)))))
